a1af63f2975efbc2baf1f1ab42e801d47cd20904
PeterDoom <nanev0205@gmail.com>
2021-02-03 22:56:55 +0200

    outsourcing the sound play functionality to a standalone function

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,25 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note1.wav');
-              },
-            ),
+          child: Column(
+            children: <Widget>[
+              FlatButton(
+                color: Colors.red,
+                onPressed: () {
+                  playSound();
+                },
+              ),
+            ],
           ),
         ),
       ),
